0ca8990c7c28c5eac8adc9247629874f067b20ea
liubao <bismy@qq.com>
2019-07-09 15:37:22 +0800

    [SCB-1364]add database implementations for UserDetailsService

--- samples/AuthenticationServer/src/main/java/org/apache/servicecomb/authentication/AuthenticationConfiguration.java
+++ samples/AuthenticationServer/src/main/java/org/apache/servicecomb/authentication/AuthenticationConfiguration.java
@@ -17,24 +17,15 @@
 
 package org.apache.servicecomb.authentication;
 
-import java.util.Arrays;
-
 import org.apache.servicecomb.authentication.token.AbstractOpenIDTokenStore;
 import org.apache.servicecomb.authentication.token.InMemoryOpenIDTokenStore;
 import org.apache.servicecomb.authentication.util.CommonConstants;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.jwt.crypto.sign.MacSigner;
 import org.springframework.security.jwt.crypto.sign.SignerVerifier;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class AuthenticationConfiguration {
@@ -54,31 +45,5 @@ public class AuthenticationConfiguration {
   public AbstractOpenIDTokenStore openIDTokenStore() {
     // NOTICE: Use in memory store for testing. Need to implement JDBC or Redis SessionIDTokenStore in product. 
     return new InMemoryOpenIDTokenStore();
-  }
-
-  @Bean(name = CommonConstants.BEAN_AUTH_USER_DETAILS_SERVICE)
-  public UserDetailsService authUserDetailsService(
-      @Autowired @Qualifier(CommonConstants.BEAN_AUTH_PASSWORD_ENCODER) PasswordEncoder passwordEncoder) {
-    // NOTICE: Use in memory UserDetails, need to implement JDBC or others in product
-    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-    UserDetails uAdmin = new User("admin", passwordEncoder.encode("changeMyPassword"),
-        Arrays.asList(new SimpleGrantedAuthority("ADMIN")));
-    UserDetails uGuest = new User("guest", passwordEncoder.encode("changeMyPassword"),
-        Arrays.asList(new SimpleGrantedAuthority("GUEST")));
-    UserDetails uGuestExpiresQuickly = new User("guestExpiresQuickly", passwordEncoder.encode("changeMyPassword"),
-        Arrays.asList(new SimpleGrantedAuthority("GUEST")));
-
-    // Third party users
-    UserDetails githubAnonymous = new User("github:anonymous", "",
-        Arrays.asList(new SimpleGrantedAuthority("GUEST")));
-    UserDetails githubLiubao68 = new User("github:liubao68", "",
-        Arrays.asList(new SimpleGrantedAuthority("ADMIN")));
-
-    manager.createUser(uAdmin);
-    manager.createUser(uGuest);
-    manager.createUser(uGuestExpiresQuickly);
-    manager.createUser(githubAnonymous);
-    manager.createUser(githubLiubao68);
-    return manager;
   }
 }
