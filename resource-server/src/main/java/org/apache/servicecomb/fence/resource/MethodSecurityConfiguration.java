e1706a64b426053e608879aec5d1cd2fc242269a
liubao <bismy@qq.com>
2024-05-15 17:32:24 +0800

    update spring security dependencies

--- resource-server/src/main/java/org/apache/servicecomb/fence/resource/MethodSecurityConfiguration.java
+++ resource-server/src/main/java/org/apache/servicecomb/fence/resource/MethodSecurityConfiguration.java
@@ -17,20 +17,16 @@
 
 package org.apache.servicecomb.fence.resource;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
-import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 
 @Configuration
-@EnableGlobalMethodSecurity(
-    prePostEnabled = true)
-public class MethodSecurityConfiguration extends GlobalMethodSecurityConfiguration {
-  @Override
-  protected MethodSecurityExpressionHandler createExpressionHandler() {
-    DefaultMethodSecurityExpressionHandler h = (DefaultMethodSecurityExpressionHandler) super.createExpressionHandler();
-    h.setDefaultRolePrefix("");
-    return h;
+@EnableMethodSecurity
+public class MethodSecurityConfiguration {
+  @Bean
+  public GrantedAuthorityDefaults grantedAuthorityDefaults() {
+    return new GrantedAuthorityDefaults("");
   }
 }
