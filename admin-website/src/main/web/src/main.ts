1c737afe4955201d842537e9665ceb454345260c
liubao <bismy@qq.com>
2024-08-15 16:00:35 +0800

    remove not used import

--- admin-website/src/main/web/src/main.ts
+++ admin-website/src/main/web/src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue';
 import * as echarts4 from 'echarts4';
-import { HwcClient } from '@opentiny/hwc-client';
 import globalComponents from '@/components';
 import router from './router';
 import store from './store';
@@ -9,20 +8,11 @@ import directive from './directive';
 import App from './App.vue';
 import '@/api/interceptor';
 import '@/assets/style/global.less';
-import config from '../hwc-exports.json';
-// eslint-disable-next-line import/extensions
 import 'echarts4/map/js/china.js';
 import chinaMap from './assets/chaina.json';
 
 echarts4.registerMap('china', chinaMap);
 const app = createApp(App);
-
-// 增加华为云相关配置
-HwcClient.configure({
-  ...config.hwcConfig,
-  accessKey: '',
-  secretKey: '',
-});
 
 app.use(router);
 app.use(store);
